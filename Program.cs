b5f584c77dd46810ec4a4d75d1723464568113ac
Val-in <valeriia.vp1@yandex.ru>
2024-08-22 08:01:48 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Tests.NUnit;
+
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            var additionalResult = calculator.Additional(5, 7);
+            Console.WriteLine("Additional: {0}", additionalResult);
+
+            var subtractionResult = calculator.Subtraction(2, 8);
+            Console.WriteLine("Subtraction: {0}", subtractionResult);
+
+            var multiplicationResult = calculator.Multiplication(6, 3);
+            Console.WriteLine("Multiplication: {0}", multiplicationResult);
+
+            var divisionResult = calculator.Division(6, 3);
+            Console.WriteLine("Division: {0}", divisionResult);
+
+            Console.ReadKey();
+        }
+    }
